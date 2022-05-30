842f3a76ff8c6a1b2f630ba00fd3344f272854eb
Anderson Pablo <anderson.pablo02@gmail.com>
2022-05-30 15:26:09 -0300

    create page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
@@ -6,11 +8,20 @@ export default function Home() {
       <Head>
         <title>igNews</title>
       </Head>
-      <h1 >
 
-        hello word
+      <main className={styles.headerContainer}>
+        <section className={styles.hero}>
+          <span>👏 hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
 
-      </h1>
+          <p>
+            bet access to all the publications <br />
+            <span>for $ 9.50 month</span>
+          </p>
+        </section>
+
+        <Image src='/images/mulher.svg' alt='logo' width={300} height={521} />
+      </main>
     </>
 
   )
